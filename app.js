87658c0e424c961c17f4c18996acc4556c15debe
Nikolay1221 <soonmydream@gmail.com>
2020-10-04 22:31:35 +0900

    version 1.1

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -37,5 +41,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+console.log('Limit file size: '+limit);
 
 module.exports = app;
