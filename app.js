5442cd4bd6391244ad978029f9c154df85986260
Nikolay1221 <soonmydream@gmail.com>
2020-10-04 22:45:26 +0900

    version 1.1z

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.set('views', path.join(__dirname, 'app_api', 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
